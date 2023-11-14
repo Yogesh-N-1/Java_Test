b01b303e34445a5ac9c15f5b34623336e6d05c9b
Yogesh <yogeshnikas7@gmail.com>
2023-11-14 16:31:48 +0530

    addtion on number statment

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("addition of two number 2+2"+" "+2+2);
+        System.out.println("addition of two number 2+2"+" "+2+2);
+
+        System.out.println("addition of two number 2+6"+" "+2+6);
+
+    }
+}
