ce13f94cc25949fe3dad5f88f5b081bd88fcdc5a
Yogesh <yogeshnikas7@gmail.com>
2023-11-14 15:15:11 +0530

    init

---
+++ Test.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("Hello I ma from java print");
+        System.out.println("Hello I ma from java ...");
+        System.out.println(new Date());
+
+    }
+
+}
